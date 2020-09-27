0f3d34e3474b5b218c0d27029ee5c4e8d8a1890c
Carl Summers <31711543+CSumm@users.noreply.github.com>
2020-09-27 19:53:03 -0400

    Add placeholder menu that will host movement, inventory, game menu and potential suspect decision makeing

---
+++ Unidentified/Assets/DisplayMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMenu : MonoBehaviour
+{
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.y <= Screen.height * 0.3)
+        {
+            canvas.SetActive(true);
+        } else
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
---
+++ Unidentified/Assets/TurnBasedMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedMovement : MonoBehaviour
+{
+    public GameObject[] rooms;
+    public GameObject[] maps;
+    float hourInSeconds = 3600;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maps[1].SetActive(false);
+        maps[2].SetActive(false);
+        maps[3].SetActive(false);
+        maps[4].SetActive(false);
+        maps[5].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time == hourInSeconds)
+        {
+            Debug.Log("game over");
+        }
+    }
+
+    public void MoveToRoom2FromRoomOne()
+    {
+   
+        rooms[1].SetActive(true);
+        maps[1].SetActive(true);
+        maps[0].SetActive(false);
+        rooms[0].SetActive(false);
+      
+    }
+
+    public void MoveToRoom1FromRoom2()
+    {
+      
+        rooms[0].SetActive(true);
+        maps[0].SetActive(true);
+        maps[1].SetActive(false);
+        rooms[1].SetActive(false);
+       
+    }
+}
